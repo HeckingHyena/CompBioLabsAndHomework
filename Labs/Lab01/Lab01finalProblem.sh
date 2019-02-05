2a8a77d845aefc08bd8e429b02ff64f90c79188a
HeckingHyena <noahtfields@gmail.com>
2019-02-05 12:39:12 -0700

    Fixing the issues with my Lab 2 problems

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,2 +1,2 @@
-$ cut -f 2-4 -d , PredPreyData.csv | head -n 1 > PredPreyDataTest.csv
-$ cut -f 2-4 -d , PredPreyData.csv |tail >> PredPreyDataTest.csv
+cut -f 2-4 -d , PredPreyData.csv | head -n 1 > PredPreyDataTest.csv
+cut -f 2-4 -d , PredPreyData.csv |tail >> PredPreyDataTest.csv
